885931dbe5cf9597d108ec637f40b60b54b36b09
vhforbes <vhforbes@gmail.com>
2024-10-06 09:29:09 -0300

    feat: some refactor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,21 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './jwt-auth.guard';
-import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthDto } from './auth.validator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  // Route for logging in using local strategy
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async login(@Request() req, @Body() body: AuthDto) {
     return this.authService.login(req.user);
   }
 
-  // A protected route using JWT Bearer token
+  // A protected route using JWT Bearer token example
   @UseGuards(JwtAuthGuard)
   @Post('profile')
   getProfile(@Request() req) {
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,8 +5,8 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { LocalStrategy } from './local.strategy';
-import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
